321af11c4c5d9d3ef5388a92d8c851ea736b14fd
Aaron Goodman <aarongoodm@gmail.com>
2020-05-26 21:54:26 -0400

    precommit tools, docstrings

--- src/corpus.py
+++ src/corpus.py
@@ -1,12 +1,14 @@
 from collections import defaultdict
 from copy import deepcopy
 import logging
+from pathlib import Path
 from typing import Iterable
 
 import networkx as nx
 from tqdm import tqdm
 
-from src.utils import words
+from src.execeptions import WordNotFound
+from src.utils.words import later_neighbors
 
 logger = logging.getLogger(__name__)
 
@@ -20,35 +22,43 @@ class Corpus:
         self._build_graphs()
 
     def _sort_words(self, words: Iterable[str]):
-        logger.info('splitting words by length')
+        logger.info("splitting words by length")
         for word in tqdm(words):
             self.words[len(word)].append(word)
-        
-        logger.info('alphabetizing words')
+
+        logger.info("alphabetizing words")
         for word_list in self.words.values():
             word_list.sort()
-    
+
     def _build_graphs(self):
         for word_len, word_list in self.words.items():
             g = nx.Graph()
-            logger.info('building nodes')
+            logger.info("building nodes")
             for word in word_list:
                 g.add_node(word)
-            
-            logger.info('building edges')
+
+            logger.info("building edges")
             for word_1 in tqdm(g.nodes):
-                for word_2 in words.later_neighbors(word_1):
+                for word_2 in later_neighbors(word_1):
                     if word_2 in g.nodes:
                         g.add_edge(word_1, word_2)
 
             self.graphs[word_len] = deepcopy(g)
 
+    def write_graphs(self, output_dir: Path):
+        output_dir.mkdir(parents=True)
+        for word_length, graph in self.graphs.items():
+            nx.readwrite.gml.write_gml(graph, output_dir / str(word_length))
+
     def get_ladder(self, word_1: str, word_2: str):
         if len(word_1) != len(word_2):
-            raise ValueError(f"Words must be the same length to make a word ladder. Got {word_1} and {word_2}")
+            raise ValueError(
+                f"Words must be the same length to make a word ladder. Got {word_1} and {word_2}"
+            )
+        if len(word_1) not in self.words.keys():
+            raise WordNotFound(f"Words {word_1} and {word_2} not in corpus")
         for word in [word_1, word_2]:
             if word not in self.words[len(word)]:
-                raise ValueError(f"Word {word} not in corpus")
+                raise WordNotFound(f"Word {word} not in corpus")
 
         return nx.shortest_path(self.graphs[len(word_1)], word_1, word_2)
-        
